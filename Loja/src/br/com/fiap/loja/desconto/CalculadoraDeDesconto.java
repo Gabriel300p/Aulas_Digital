212aade8bf8b1fde80fe45e5aa81f5dabc4df387
Gabriel300p <gabriel.andrade200p@gmail.br>
2022-03-03 21:35:20 -0300

    Aula 01 - Strategy

---
+++ Loja/src/br/com/fiap/loja/desconto/CalculadoraDeDesconto.java
@@ -0,0 +1,21 @@
+package br.com.fiap.loja.desconto;
+
+import java.math.BigDecimal;
+
+import br.com.fiap.loja.Pedido;
+
+public class CalculadoraDeDesconto {
+	
+	public BigDecimal calcular(Pedido pedido) {
+		
+		if (pedido.getValor().compareTo(new BigDecimal(500)) > 0) {
+			return pedido.getValor().multiply(new BigDecimal(0.1));
+		} 	
+		
+		if (pedido.getTotalDeItens () > 10) {
+			return pedido.getValor().multiply(new BigDecimal(0.5));
+		}
+		return BigDecimal.ZERO;	
+	}
+
+}
